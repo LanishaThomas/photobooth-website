f159e18475c7a3b7e9ebb2a5ed24095ed22ea86d
LanishaThomas <lanishathomas7606@gmail.com>
2026-01-15 12:30:00 +0530

    Disable sourcemaps to satisfy CSP on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/photobooth-website/"
+  base: "/photobooth-website/",
+  build: {
+    sourcemap: false
+  }
 })
